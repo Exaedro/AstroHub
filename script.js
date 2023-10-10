dca12b8cbf7cff236707703105897b67a78216bf
exaedro <owomolo123@gmail.com>
2023-10-10 13:29:22 -0300

    script.js actualizado

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const fraseh1 = document.getElementById('frase')
 const frases = ['Incluso las estrellas más brillantes se queman al final.', 'Todo lo que vemos de las estrellas son sus viejas fotografías.', 'Yo, un universo de átomos, un átomo en el universo.', 'No me sentia como un gigante. Me sentí muy, muy pequeño.']
-const aleatorio = frases[Math.floor(Math.random() * frases.length)]
 
-fraseh1.innerHTML = aleatorio;
+const separarFrases = frases.map(frase => {
+    return frase.split(' ')
+})
+
+const aleatorio = separarFrases[Math.floor(Math.random() * frases.length)]
+aleatorio.forEach(palabra => {
+    let span = document.createElement('span')
+    span.className = 'remarcar-palabra'
+    span.innerHTML = palabra
+    span.style.display = 'inline-block'
+    span.style.marginRight = '15px'
+    fraseh1.appendChild(span)
+})
